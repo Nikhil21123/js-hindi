580603fb8b82a1054031810edc629ae969bbc5c3
Nikhil <siddhapuranik@gmail.com>
2026-03-26 10:25:48 +0000

    datatypes

--- .github/01_basics/02_datatypes.js
+++ .github/01_basics/02_datatypes.js
@@ -2,5 +2,21 @@
 
 // alert("HI"); //we are using node js, not browser
 
-console.log(3 + 3);
-console.log("Nikhil");
+//console.log(3 + 3);
+//console.log("Nikhil");
+
+let name="Nikhil"       //string use in ' ' or " "
+let age=18              //number range = 2 to pow 53
+let isLoggedin = true   //boolean true or false
+let state;              //undefined not define a value
+let symbol              //unique
+let a=null              // standalone value
+//bigint
+//object
+
+console.table([name,age,isLoggedin,state,symbol,a])
+console.log(typeof "nikhil")
+console.log(typeof name)
+console.log(typeof null)
+console.log(typeof undefined)
+console.log(typeof age, age)
