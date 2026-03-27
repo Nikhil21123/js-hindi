e28b83adde348a481c9d3cc4c5bca39d25532c03
Nikhil <siddhapuranik@gmail.com>
2026-03-27 17:32:48 +0000

    all

---
+++ .github/01_basics/06_math.js
@@ -0,0 +1,15 @@
+// const num=new Number(100)
+// console.log(num)
+// console.log(num.toString().length)
+// console.log(num.toFixed(2))
+
+// *********************toPrecision*************************
+// const num=23.8966
+// console.log(num.toPrecision(4))
+
+// *********************toLocaleString**********************
+// const num1 = 1000000
+// console.log(num1.toLocaleString())
+// console.log(num1.toLocaleString('en-IN'))
+
+
