fa8cef2c763399bd88ddce8f9e7f6dda659a64fe
Nikhil <siddhapuranik@gmail.com>
2026-04-13 10:38:31 +0000

    06_math.js

--- .github/01_basics/06_math.js
+++ .github/01_basics/06_math.js
@@ -22,4 +22,10 @@
 // console.log(Math.min(4,5,8,1,2,4,7))
 // console.log(Math.max(1,5,8,7,4,9,5,8,5,85,8,58,5,8))
 
-console.log(Math.random())
+// console.log(Math.random())
+// console.log((Math.random() * 10) + 1)
+// console.log(Math.floor(Math.random() * 10) + 1)
+
+// min = 20
+// max = 10
+// console.log(Math.floor(Math.random() * (max - min + 1)) + min)
