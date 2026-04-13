7c087ec79ea68a089a2bd175dbfec900d381a589
Nikhil <siddhapuranik@gmail.com>
2026-04-13 10:36:40 +0000

    math

--- .github/01_basics/06_math.js
+++ .github/01_basics/06_math.js
@@ -12,4 +12,14 @@
 // console.log(num1.toLocaleString())
 // console.log(num1.toLocaleString('en-IN'))
 
+// ********************** Maths ****************************
+// console.log(Math)
+// console.log(Math.abs(-3))
+// console.log(Math.round(3.2))
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.2))
+// console.log(Math.sqrt(4))
+// console.log(Math.min(4,5,8,1,2,4,7))
+// console.log(Math.max(1,5,8,7,4,9,5,8,5,85,8,58,5,8))
 
+console.log(Math.random())
