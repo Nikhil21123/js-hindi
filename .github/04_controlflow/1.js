ef1887cb2dae460f078a9d101dcaea891dc07880
Nikhil <siddhapuranik@gmail.com>
2026-04-21 14:26:46 +0000

    1.js

---
+++ .github/04_controlflow/1.js
@@ -0,0 +1,44 @@
+// if
+
+// const user = false;
+// if(2 != 2){
+//  console.log("this is true")
+// }
+// else{
+
+// }
+
+// <, >, <=, >=, ==, !=, === (check type and execute), !==, 
+
+// const score = 200
+// if(score>100){
+//     const power = "fly" 
+//     console.log(`usr power ${power}`)
+// }
+
+
+// const balance = 1000;
+// if(balance < 500) console.log("execute")
+//     else{
+//             console.log("nt ecexute")
+//     }
+
+
+// const balance = 1000
+// if(balance < 500){
+//     console.log("less than")
+// }else if(balance < 750){
+//     console.log("less than 500")
+// }
+// else if(balance < 900){
+//     console.log("less than 750")
+// }else{
+//     console.log("less than 1200")
+// }
+
+const user = true
+const debit = true 
+const userislogein = false
+if(user && debit || userislogein){
+    console.log("logged in and buy")
+}
