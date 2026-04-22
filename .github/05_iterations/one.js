18bb0af6ccfd0c84df756e0d23d4076520859d03
Nikhil <siddhapuranik@gmail.com>
2026-04-22 07:20:05 +0000

    one.js

---
+++ .github/05_iterations/one.js
@@ -0,0 +1,10 @@
+// for
+
+for (let i = 0; i<=10; i++){
+// console.log(i);
+}
+const array = [1,2,3]
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    console.log(array)
+}
